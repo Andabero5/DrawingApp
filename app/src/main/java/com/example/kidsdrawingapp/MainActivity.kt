bde0f328f98b41f89174acf01b41fa15dab3c988
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2022-05-03 10:15:36 -0500

    static draw

---
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kidsdrawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kidsdrawingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.drawingView.setSizeForBrush(20F)
+
+    }
+}
